968133979fed8832e9429b21bc07849d09c83264
maaja <01161548@pw.edu.pl>
2023-01-05 21:48:49 +0100

    add project

---
+++ store/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price',
+                    'is_stock', 'created', 'updated']
+    list_filter = ['is_stock', 'is_active']
+    list_editable = ['price', 'is_stock']
+    prepopulated_fields = {'slug': ('title',)}
