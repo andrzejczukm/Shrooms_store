5f9683b9d5471b1b8aa2e18a963acb09949ecf29
maaja <01161548@pw.edu.pl>
2023-01-23 15:49:01 +0100

    delete password to email

--- core/settings.py
+++ core/settings.py
@@ -135,12 +135,15 @@ AUTH_USER_MODEL = 'account.UserBase'
 LOGIN_REDIRECT_URL = '/account/dashboard'
 LOGIN_URL = '/account/login/'
 
+# w ten sposob maila przychodza do consoli
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shroomstore1@gmail.com'
-EMAIL_HOST_PASSWORD = 'xuhewxwfdyngrujg'
+# usunelam haslo, żeby nikt nie mial dostepu do maila 
+EMAIL_HOST_PASSWORD = 'tu_wpisujemy_haslo_do_aplikacji'
+
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
